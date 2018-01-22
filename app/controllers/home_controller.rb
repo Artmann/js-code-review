65727500ed0cf646a134a51de7ed96fe36c3f8c6
Christoffer Artmann <christoffer@artmann.co>
2018-01-22 20:30:55 +0100

    Add a basic front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @review_requests = ReviewRequest.order("created_at DESC").to_a.uniq { |s| s.user_id }.first(8)
+
+    @reviewers = User.all.to_a.shuffle.first(8)
   end
 end

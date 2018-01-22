4badaee45a351d88b4f76dcf507338f917d401cd
Christoffer Artmann <christoffer@artmann.co>
2018-01-22 20:40:51 +0100

    Load react components directly instead of in

--- app/javascript/packs/review_request.js
+++ app/javascript/packs/review_request.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import ReviewRequest from '../src/components/review_request';
 
-document.addEventListener('turbolinks:load', () => {
-  let container = document.getElementById('review-request');
-  let files = JSON.parse(container.getAttribute('data-files'));
+let container = document.getElementById('review-request');
+let files = JSON.parse(container.getAttribute('data-files'));
 
-  ReactDOM.render(
-    <ReviewRequest files={files}/>,
-    container
-  );
-});
+ReactDOM.render(
+  <ReviewRequest files={files}/>,
+  container
+);
--- app/javascript/packs/review_request_form.js
+++ app/javascript/packs/review_request_form.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import ReviewRequestForm from '../src/components/review_request_form';
 
-document.addEventListener('turbolinks:load', () => {
-  ReactDOM.render(
-    <ReviewRequestForm/>,
-    document.getElementById('review-request-form')
-  );
-});
+ReactDOM.render(
+  <ReviewRequestForm/>,
+  document.getElementById('review-request-form')
+);

2cd3ef133ba66ecb4bf5c16cf66c980550ebd6d7
Christoffer Artmann <christoffer@artmann.co>
2018-01-22 21:03:55 +0100

    Give the front page some color

--- app/javascript/src/tailwind.js
+++ app/javascript/src/tailwind.js
@@ -91,7 +91,7 @@ let colors = {
   'teal-darkest': '#0d3331',
   'teal-darker': '#20504f',
   'teal-dark': '#38a89d',
-  'teal': '#4dc0b5',
+  'teal': '#4abdac',
   'teal-light': '#64d5ca',
   'teal-lighter': '#a0f0ed',
   'teal-lightest': '#e8fffe',
